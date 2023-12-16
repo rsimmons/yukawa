3d79f27814519790d5ba707a876f8f7297f4d9f1
Russel Simmons <russ@rsimmons.org>
2023-12-16 08:59:06 -0800

    Clean subs, find sub files, extract clips

--- clip/clip.py
+++ clip/clip.py
@@ -5,6 +5,7 @@ import subprocess
 import tempfile
 import pprint
 import warnings
+from pathlib import Path
 
 import srt
 import whisper
@@ -12,16 +13,38 @@ import diff_match_patch as dmp
 
 from ja import JapaneseAnalyzer
 
-print('loading Whisper model...')
-whisper_model = whisper.load_model('large-v3')
-print('done')
-
 FORCE_BREAK_TIME = 2
 MAX_CLIP_LENGTH = 10
 
 def extract_audio(vid_fn, start_time, end_time, audio_fn):
     # extract to wav to avoid re-encoding
     cmdline = ['ffmpeg', '-ss', str(start_time.total_seconds()), '-accurate_seek', '-i', vid_fn, '-t', str((end_time - start_time).total_seconds()), '-map', '0:a:0', '-ac', '1', '-acodec', 'pcm_s16le', '-y', audio_fn]
+    with open(os.devnull, 'w') as devnull:
+        subprocess.check_call(cmdline, stderr=devnull)
+
+def extract_video(vid_fn, start_time, end_time, out_fn):
+    OUTPUT_WIDTH = 854
+    OUTPUT_HEIGHT = 480
+    cmdline = [
+        'ffmpeg',
+        '-ss', str(start_time.total_seconds()),
+        '-accurate_seek',
+        '-i', vid_fn,
+        '-t', str((end_time - start_time).total_seconds()),
+        # make correct width and height, padding with black in one dimension if necessary. from https://superuser.com/a/547406
+        #'-vf', 'scale=(sar*iw)*min({width}/(sar*iw)\\,{height}/ih):ih*min({width}/(sar*iw)\\,{height}/ih),pad={width}:{height}:({width}-(sar*iw)*min({width}/(sar*iw)\\,{height}/ih))/2:({height}-ih*min({width}/(sar*iw)\\,{height}/ih))/2,setdar=16:9'.format(width=OUTPUT_WIDTH, height=OUTPUT_HEIGHT),
+        '-vf', 'scale={width}:{height}:force_original_aspect_ratio=decrease,pad={width}:{height}:-1:-1:color=black,setdar=16:9,setsar=1'.format(width=OUTPUT_WIDTH, height=OUTPUT_HEIGHT),
+        '-ac', '2',
+    ]
+    if out_fn.endswith('.mp4'):
+        cmdline += ['-strict', '-2', '-acodec', 'aac', '-vcodec', 'h264', '-f', 'mp4']
+    elif out_fn.endswith('.webm'):
+        cmdline += ['-acodec', 'libvorbis', '-vcodec', 'libvpx', '-crf', '10', '-b:v', '1M', '-f', 'webm']
+    else:
+        assert False, 'unknown output format'
+
+    cmdline += ['-y', out_fn]
+
     with open(os.devnull, 'w') as devnull:
         subprocess.check_call(cmdline, stderr=devnull)
 
@@ -114,10 +137,28 @@ def process(vid_fn, sub_fn, analyzer):
 
     subs = list(srt.parse(sub_data))
 
+    cleaned_subs = []
+    for sub in subs:
+        clean_text = analyzer.clean_text(sub.content)
+        if clean_text != sub.content:
+            print('BEGIN CLEANED SUBTITLE:')
+            print(sub.content)
+            print('---')
+            print(clean_text)
+            print('END CLEANED SUBTITLE')
+
+            if not clean_text:
+                print('SKIPPING EMPTY SUBTITLE')
+                print()
+                continue
+
+            print()
+        cleaned_subs.append(srt.Subtitle(sub.index, sub.start, sub.end, clean_text))
+
     coarse_groups = []
     group = []
     prev_end = None # time the last subtitle ended
-    for sub in subs:
+    for sub in cleaned_subs:
         if (prev_end is None) or ((sub.start - prev_end) < datetime.timedelta(seconds=FORCE_BREAK_TIME)):
             group.append(sub)
         else:
@@ -127,9 +168,21 @@ def process(vid_fn, sub_fn, analyzer):
     if group:
         coarse_groups.append(group)
 
+    OUTDIR = './output'
+    Path(OUTDIR).mkdir(parents=True, exist_ok=True)
     sims = []
+    clip_count = 0
     try:
         for group in coarse_groups:
+            # print('BEGIN COARSE GROUP')
+            # for sub in group:
+            #     print('--')
+            #     print(sub.content)
+            # print('--')
+            # print('END COARSE GROUP')
+            # print()
+            # print()
+
             for clip_subs in divide_group(group):
                 clip_start = clip_subs[0].start
                 clip_end = clip_subs[-1].end
@@ -149,18 +202,26 @@ def process(vid_fn, sub_fn, analyzer):
                         whisper_result = whisper_model.transcribe(audio_fn, language='ja', initial_prompt='映画の字幕です。')
                     # pprint.pprint(whisper_result)
 
-                human_text = '\n'.join(sub.content for sub in clip_subs)
+                human_text = '\n'.join(analyzer.clean_text(sub.content) for sub in clip_subs)
                 asr_text = whisper_result['text']
 
+                print('CLEANED HUMAN TEXT:')
+                print(human_text)
                 print('ASR TEXT:', asr_text)
 
                 sim = text_similarity(analyzer, human_text, asr_text)
                 sims.append(sim)
                 print('SIMILARITY:', sim)
 
+                clip_fn = os.path.join(OUTDIR, f'{clip_count:04d}.mp4')
+                extract_video(vid_fn, clip_start, clip_end, clip_fn)
+                print('CLIP FILE:', clip_fn)
+
                 print('END CLIP')
                 print()
                 print()
+
+                clip_count += 1
     except KeyboardInterrupt:
         print('INTERRUPTED')
 
@@ -168,7 +229,54 @@ def process(vid_fn, sub_fn, analyzer):
     for thresh in [0.9, 0.8, 0.5]:
         print(f'similarity above {thresh}:', sum(1 for sim in sims if sim > thresh) / len(sims))
 
-# TODO: don't update candidate split index if first sub ends with continuation arrow
+BCP_ALT_SUB_CODES = {
+    'ja': ['jpn', 'jp'],
+}
 
-ja_analyzer = JapaneseAnalyzer()
-process('SpiritedAway.mp4', 'SpiritedAway.srt', ja_analyzer)
+def find_matching_subfile(vid_fn, bcp):
+    allow_codes = [bcp] + BCP_ALT_SUB_CODES.get(bcp, [])
+
+    vid_dir, vid_basename = os.path.split(os.path.abspath(vid_fn))
+    vid_prefix, vid_ext = os.path.splitext(vid_basename)
+    assert vid_ext in ['.mp4', '.mkv', '.webm']
+
+    potential_sub_fns = [] # list of (code, fn) tuples
+    for sub_fn in os.listdir(vid_dir):
+        if sub_fn.startswith(vid_prefix):
+            sub_prefix, sub_ext = os.path.splitext(sub_fn)
+            if sub_ext == '.srt':
+                sub_mid = sub_prefix[len(vid_prefix):]
+                sub_code = ''.join(c for c in sub_mid if c.isalpha())
+                if (not sub_code) or (sub_code.lower() in allow_codes):
+                    potential_sub_fns.append((sub_code, sub_fn))
+
+    if len(potential_sub_fns) == 0:
+        return None
+    if len(potential_sub_fns) == 1:
+        return os.path.join(vid_dir, potential_sub_fns[0][1])
+    else:
+        assert False, 'multiple potential subtitle files found'
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('usage: clip.py <video file>')
+        sys.exit(1)
+
+    vid_fn = sys.argv[1]
+    if not os.path.exists(vid_fn):
+        print('video file does not exist')
+        sys.exit(1)
+
+    bcp = 'ja'
+    sub_fn = find_matching_subfile(vid_fn, bcp)
+    if sub_fn is None:
+        print('could not find matching subtitle file')
+        sys.exit(1)
+    print('found subtitle file:', sub_fn)
+
+    print('loading Whisper model...')
+    whisper_model = whisper.load_model('large-v3')
+    print('done')
+
+    ja_analyzer = JapaneseAnalyzer()
+    process(vid_fn, sub_fn, ja_analyzer)
--- clip/ja.py
+++ clip/ja.py
@@ -1,3 +1,5 @@
+import re
+
 from sudachipy import tokenizer, dictionary
 
 # key is (reading_form, normalized_form), value is new reading_form
@@ -33,8 +35,20 @@ def _get_morpheme_token(m):
 
     return rf
 
-def _clean_text(text):
-    return text.replace('～', '')
+# clean text so as represent spoken language, removing things like descriptions
+# of sounds, speaker names, etc.
+def _user_clean_text(text):
+    # remove parentheses and their contents
+    text = re.sub(r'（[^）]*）', '', text)
+    text = re.sub(r'\([^)]*\)', '', text)
+    text = text.strip()
+    return text
+
+# clean text in a way that helps the tokenizer
+def _tokenization_clean_text(text):
+    text = text.replace('～', '') # phonetic prolongation mark
+
+    return text
 
 class JapaneseAnalyzer:
     bcp = 'ja'
@@ -42,7 +56,27 @@ class JapaneseAnalyzer:
     def __init__(self):
         self.sudachi_tokenizer_obj = dictionary.Dictionary().create()
 
+    def clean_text(self, text):
+        return _user_clean_text(text)
+
     def make_tokenstr(self, text):
-        text = _clean_text(text)
+        text = _tokenization_clean_text(text)
         morphemes = self.sudachi_tokenizer_obj.tokenize(text, tokenizer.Tokenizer.SplitMode.B)
         return ' '.join(_get_morpheme_token(m) for m in morphemes if not _ignore_morpheme(m))
+
+USER_CLEANUP_TESTS = [
+    # fullwidth parentheses
+    ('（井之頭(いのがしら)五郎(ごろう)）\n何年ぶりだ？', '何年ぶりだ？'),
+    ('（物が落ちる音）\n（おばあちゃん）あっ！', 'あっ！'),
+    ('（おばあちゃんが力む息）', ''),
+    ('（おばあちゃん）\nあっ', 'あっ'),
+    ('（五郎）あっ いえ\n（店長）わざわざ すいませんねえ', 'あっ いえ\nわざわざ すいませんねえ'),
+
+    ('（五郎）\n信玄袋(しんげんぶくろ)？', '信玄袋？'), # fullwidth parentheses and ascii parentheses
+]
+
+if __name__ == '__main__':
+    for (text, clean_text) in USER_CLEANUP_TESTS:
+        output = _user_clean_text(text)
+        print(repr(text), repr(output), repr(clean_text))
+        assert output == clean_text
