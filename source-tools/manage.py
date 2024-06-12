bafd01ef76ff1a34b33b4e8895a553f57d0a5f77
Russel Simmons <russ@rsimmons.org>
2024-06-11 20:47:49 -0700

    Add tool to derive a progression from full source archive

--- source-tools/manage.py
+++ source-tools/manage.py
@@ -1,9 +1,14 @@
 import argparse
 import json
 from pathlib import Path
+import itertools
+import re
+from collections import Counter
 
 import webvtt
 import requests
+import spacy
+from spacy.lang.es import Spanish
 
 from index_settings import INDEX_SETTINGS
 
@@ -108,6 +113,68 @@ def index_captions(archive_dir, lang):
     resp = requests.post(f'http://localhost:9200/{index}/_refresh')
     resp.raise_for_status()
 
+RE_WORD = re.compile(r'\w+')
+
+def get_normalized_words_es(text):
+    result = []
+    for m in RE_WORD.finditer(text):
+        norm = m.group().lower()
+        result.append(norm)
+    return result
+
+def analyze_captions(archive_dir, lang, limit):
+    if lang == 'es':
+        nlp = Spanish()
+        nlp.add_pipe("sentencizer")
+    else:
+        assert False
+
+    word_freq = {} # currently these are lowercased but not stemmed/lemmatized
+
+    print('determining token frequencies')
+    for vid_info in itertools.islice(iter_videos(archive_dir, archive_dir / lang, lang), limit):
+        if 'captions' in vid_info:
+            for caption in vid_info['captions']:
+                for word in get_normalized_words_es(caption['text']):
+                    word_freq[word] = word_freq.get(word, 0) + 1
+
+    ordered_word_freqs = sorted(word_freq.items(), key=lambda x: -x[1])
+    word_rank = {word: i+1 for (i, (word, _)) in enumerate(ordered_word_freqs)}
+
+    print('finding sentence ranks')
+    ranked_sentences = []
+    for vid_info in itertools.islice(iter_videos(archive_dir, archive_dir / lang, lang), limit):
+        if 'captions' in vid_info:
+            joined_captions = '\n'.join(caption['text'] for caption in vid_info['captions'])
+            doc = nlp(joined_captions)
+            for sent in doc.sents:
+                clean_sent_text = sent.text.replace('​', ' ').strip().replace('\n', ' ').lstrip('- ').lstrip('-').rstrip('¿').rstrip('¡')
+                if not clean_sent_text:
+                    continue
+                words = get_normalized_words_es(clean_sent_text)
+                if not words:
+                    continue
+                highest_rank = max(word_rank[word] for word in words)
+                ranked_sentences.append((highest_rank, clean_sent_text))
+
+    ranked_sentences.sort()
+    current_rank = 0
+    prog = []
+    word_num = 0
+    for rank, text in ranked_sentences:
+        if rank > current_rank:
+            new_word = ordered_word_freqs[rank-1][0]
+            current_rank = rank
+            word_num += 1
+            prog.append({'word': new_word, 'rank': rank, 'word_num': word_num, 'sent_count': Counter()})
+        prog[-1]['sent_count'][text] += 1
+
+    for step in prog:
+        print()
+        print(f'{step["word"]} #{step["word_num"]} (rank {step["rank"]})')
+        for sent, count in step['sent_count'].most_common():
+            print(f'  ({count}) {sent}')
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
@@ -117,9 +184,16 @@ if __name__ == '__main__':
     parser_index_subs.add_argument('archive_dir', type=Path)
     parser_index_subs.add_argument('lang')
 
+    parser_analyze_captions = subparsers.add_parser('analyze_captions')
+    parser_analyze_captions.add_argument('archive_dir', type=Path)
+    parser_analyze_captions.add_argument('lang')
+    parser_analyze_captions.add_argument('--limit', type=int)
+
     args = parser.parse_args()
 
     if args.command == 'index_captions':
         index_captions(args.archive_dir, args.lang)
+    elif args.command == 'analyze_captions':
+        analyze_captions(args.archive_dir, args.lang, args.limit)
     else:
         raise Exception('invalid command')
